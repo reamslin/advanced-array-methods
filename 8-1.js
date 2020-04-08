05dcf6effda3ff8200eab9e272f6f120842e5949
reamslin <reamslin@gmail.com>
2020-04-08 00:49:55 -0400

    first commit

---
+++ 8-1.js
@@ -0,0 +1,34 @@
+function doMath(x, y, f) {
+    f(x, y);
+}
+
+function doAllMath(a,b,fs) {
+    for (let f of fs) {
+        f(a, b);
+    }
+}
+
+function myForEach(arr, callback) {
+    for (let i = 0; i < arr.length; i++) {
+        callback(arr[i], i, arr);
+    
+    }
+}
+
+function myMap(arr, callback) {
+    let newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        newArray.push(callback(arr[i], i, arr));
+    }
+    return newArray;
+}
+
+function myFilter(arr, callback) {
+    let newArray = [];
+    for (let i = 0; i < arr.length; i ++) {
+        if (callback(arr[i], i, arr)) {
+            newArray.push(arr[i]);
+        }
+    }
+    return newArray;
+}
